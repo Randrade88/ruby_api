222b4595484197f21d69742c1743fe8922a385d8
Ronaldo <naldowork@gmail.com>
2023-12-06 18:05:34 -0300

    remove

--- db/migrate/20231123010806_users.rb
+++
@@ -1,9 +0,0 @@
-class Users < ActiveRecord::Migration[7.1]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :password
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20231123015053_create_users.rb
+++
@@ -1,4 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[7.1]
-  def change
-  end
-end
